c3b0c4ce74d986e86302d70c390255a32d6da40e
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-02-23 15:59:33 +0200

    Create 03. Class Book.py

---
+++ OOP/1. First Steps in OOP/First Steps in OOP - Lab/03. Class Book.py
@@ -0,0 +1,15 @@
+class Book:
+
+    def __init__(self, name, author, pages):
+        self.name = name
+        self.author = author
+        self.pages = pages
+
+    def name(self):
+        return self.name
+
+    def author(self):
+        return self.author
+
+    def pages(self):
+        return self.pages
