682be030ba51fe3a05e7c8a7b858a2d4212347aa
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-01 18:52:52 +0200

    Update zoo.py

--- OOP/4.Encapsulation/Encapsulation - Exercise/01.Wild Cat Zoo/zoo.py
+++ OOP/4.Encapsulation/Encapsulation - Exercise/01.Wild Cat Zoo/zoo.py
@@ -1,3 +1,5 @@
+from project.animal import Animal
+
 class Zoo:
   def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
     self.name = name
@@ -5,7 +7,25 @@ class Zoo:
     self.__animal_capacity = animal_capacity
     self._workers_capacity = workers_capacity
     self.animals = []
-    self..workers = []
+    self.workers = []
 
-   def add_animal(self, animal, price):  
+   def add_animal(self, animal: Animal, price):
+     if self.__budget < price:
+       return "Not enough budget"
+     if self.__animal_capacity == len(self.animals):
+       return "Not enough space for animal"
+
+     self.animals.append(animal)
+     self.__budget -= price
+
+     return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+
+    def hire_worker(self, worker):
+      
+
+     
+     
+       
+       
+     
   
