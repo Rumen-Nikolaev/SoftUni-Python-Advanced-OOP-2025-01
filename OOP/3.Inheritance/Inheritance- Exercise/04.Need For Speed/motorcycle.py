1d6234187ba8fc695d3d7e930b916920c63cc0ea
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-01 13:12:13 +0200

    Create motorcycle.py

---
+++ OOP/3.Inheritance/Inheritance- Exercise/04.Need For Speed/motorcycle.py
@@ -0,0 +1,8 @@
+from project.vehicle import Vehicle
+
+class Motorcycle:
+  def drive(self, kilometers):
+        if self.fuel - (kilometers * Motorcycle.fuel_consumption) >= 0:
+            self.fuel -= (kilometers * Motorcycle.fuel_consumption)
+            
+  
