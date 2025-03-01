f0c217cb672d0391664c5c13c92e00b6b8c9522f
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-01 15:35:40 +0200

    Create zoo.py

---
+++ OOP/4.Encapsulation/Encapsulation - Exercise/01.Wild Cat Zoo/zoo.py
@@ -0,0 +1,11 @@
+class Zoo:
+  def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
+    self.name = name
+    self.__budget = budget
+    self.__animal_capacity = animal_capacity
+    self._workers_capacity = workers_capacity
+    self.animals = []
+    self..workers = []
+
+def 
+  
