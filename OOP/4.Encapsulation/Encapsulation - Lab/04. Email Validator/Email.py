e0b8fa71970c4f311d340592c38999d084c3f9cc
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-01 18:27:07 +0200

    Update Email.py

--- OOP/4.Encapsulation/Encapsulation - Lab/04. Email Validator/Email.py
+++ OOP/4.Encapsulation/Encapsulation - Lab/04. Email Validator/Email.py
@@ -1,1 +1,26 @@
+class EmailValidator:
+  def __init__(self, min_length, mails, domains):
+    self.min_length = min_length
+    self.mails = mails
+    self.domains = domains
 
+   def __is_name_valid(self, name):
+    return len(name) >= self.min_length
+
+   def __is__mail__valid(self, mail):
+    return mail in self.mails
+
+   def __is_domain_valid(self, domain):
+     return domain in self.domains
+
+   
+    def validate(self, email):
+      _username = email.split("@")[0]
+      _mail = email.split("@")[1].split(".")[0]
+      _domain = email.split(".")[-1]
+
+    valid_name = self.__is_name_valid(_username)
+    valid_mail = self.__is__mail_valid(_mail)
+    valid_domail = self.__is__domail__valid(_domain)
+
+    return valid_name and valid_mail and valid_domain
