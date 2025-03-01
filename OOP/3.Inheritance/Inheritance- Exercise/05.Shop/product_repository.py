b4afcf0ec4fbcc8cf4f0d61ec73e0b52f39a2ffa
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-01 18:11:57 +0200

    Update product_repository.py

--- OOP/3.Inheritance/Inheritance- Exercise/05.Shop/product_repository.py
+++ OOP/3.Inheritance/Inheritance- Exercise/05.Shop/product_repository.py
@@ -3,5 +3,24 @@ from project.product import Product
 class ProductRepository:
   product = []
 
-def add(self, product):
-  ProductRepository.product.append(product)
+  def add(self, product: Product):
+    ProductRepository.product.append(product)
+
+  def find(self, product: Product):
+    for x in self.products:
+      if x.__class__.__name__ == product.__class__.__name__:
+        return x
+
+     def remove(self, product_name):
+        for pos, x in enumerate(self.products):
+            if x.name == product_name:
+                del self.products[pos]
+
+    def __repr__(self):
+        show = ""
+        for x in self.products:
+            if x.name not in show:
+                show += f"{x.name}: {x.quantity}\n"
+        return show.rstrip()
+     
+  
