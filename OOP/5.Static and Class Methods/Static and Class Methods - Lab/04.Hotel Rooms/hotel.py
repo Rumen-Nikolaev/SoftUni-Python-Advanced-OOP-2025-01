b37af9bea4dd93c82d0149b91570046034668b61
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-02 17:12:38 +0200

    Update hotel.py

--- OOP/5.Static and Class Methods/Static and Class Methods - Lab/04.Hotel Rooms/hotel.py
+++ OOP/5.Static and Class Methods/Static and Class Methods - Lab/04.Hotel Rooms/hotel.py
@@ -1,7 +1,19 @@
+from project.room import Room
+
 class Hotel:
   def __init__(self, name):
     self.name = name
     self.rooms = []
     self.guests = 0
 
-  def from_stars(
+  @classmethod
+  def from_stars(cls, stars_count):
+    return cls(f"{stars_count} stars Hotel")
+
+  def add_room(self, room: Room):
+    self.rooms.append(room)
+
+  def take_room(self, room_number, people):
+    f
+    
+    
