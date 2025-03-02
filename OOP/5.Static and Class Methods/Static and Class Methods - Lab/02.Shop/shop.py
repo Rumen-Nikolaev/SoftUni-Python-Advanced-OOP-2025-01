71a250c4ab8cac15e1c192ec2305cbe5234c91df
Rumen Nikolaev <118046653+Rumen-Nikolaev@users.noreply.github.com>
2025-03-02 15:13:07 +0200

    Update shop.py

--- OOP/5.Static and Class Methods/Static and Class Methods - Lab/02.Shop/shop.py
+++ OOP/5.Static and Class Methods/Static and Class Methods - Lab/02.Shop/shop.py
@@ -4,3 +4,22 @@ class Shop:
     self.type = type
     self.capacity = capacity
     self.items = {}
+
+  @classmethod
+  def small_shop(cls, name, type_shop):
+    return cls(name, type_shop, 10)
+
+  def add_item(self, item_name):
+    if sum(self.items.values()) == self.capacity:
+      return "Not enough capacity in the shop"
+
+    if item_name not in self.items:
+      self.items[item_name] = 0
+
+    self.items[item_name] += 1
+
+    return f"{item_name} added to the shop"
+
+    def remove_item(self, item_name, amount):
+      
+    
